38bd871c544f3f5a38cbe290f55fd7048a8f4300
Doga Tekin <doga.tekin@epfl.ch>
2020-07-13 22:59:11 +0300

    Chevron cursor pointer

--- about/script.js
+++ about/script.js
@@ -4,8 +4,8 @@ $('.caro').slick({
   centerPadding: '50px',
   slidesToShow: 3,
   focusOnSelect: true,
-  prevArrow: '<i style="position: absolute; top: 50%; transform: translate(0, -50%); left: -35px;" class="fas fa-chevron-left fa-2x"></i>',
-  nextArrow: '<i style="position: absolute; top: 50%; transform: translate(0, -50%); right: -35px;" class="fas fa-chevron-right fa-2x"></i>',
+  prevArrow: '<i style="position: absolute; top: 50%; transform: translate(0, -50%); left: -35px; cursor: pointer;" class="fas fa-chevron-left fa-2x"></i>',
+  nextArrow: '<i style="position: absolute; top: 50%; transform: translate(0, -50%); right: -35px; cursor: pointer;" class="fas fa-chevron-right fa-2x"></i>',
   responsive: [{
       breakpoint: 768,
       settings: {
